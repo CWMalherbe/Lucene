ea49770097067ac1e63f61a8250e02bd47a5fa67
Charl Malherbe <Charl.Malherbe@avisbudget.co.za>
2017-10-10 17:37:40 +0200

    Add project files.

---
+++ Lucene.Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lucene.Mvc.MvcApplication" Language="C#" %>
---
+++ Lucene.Mvc/Models/LuceneSearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.QueryParsers;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+using Version = Lucene.Net.Util.Version;
+using System.IO;
+
+
+namespace Lucene.Mvc.Models
+{
+    public static class LuceneSearch
+    {
+        private static string _luceneDir = Path.Combine(HttpContext.Current.Request.PhysicalApplicationPath, "lucene_index");
+        private static FSDirectory _directoryTemp;
+        private static FSDirectory _directory
+        {
+            get
+            {
+                if (_directoryTemp == null)
+                    _directoryTemp = FSDirectory.Open(new DirectoryInfo(_luceneDir));
+                if (IndexWriter.IsLocked(_directoryTemp))
+                    IndexWriter.Unlock(_directoryTemp);
+                var lockFilePath = Path.Combine(_luceneDir, "write.lock");
+                if (File.Exists(lockFilePath))
+                    File.Delete(lockFilePath);
+                return _directoryTemp;
+            }
+        }
+    }
+}
